0b163d78994c74029981beac00ac3bf0329d0b04
Anon <92399089+zaphir-code@users.noreply.github.com>
2022-07-22 18:24:24 +0300

    Update NoTASBOT.cpp

--- NoTASBOT.cpp
+++ NoTASBOT.cpp
@@ -2,13 +2,11 @@
 
 DWORD WINAPI Main_Thread(LPVOID instance)
 {
-	MessageBoxA(NULL, "Test messadsge", "Test label", MB_OK);
 	DWORD tBotBase = (DWORD)GetModuleHandleA("tBot.dll");
 	char(*tBotLabel)[16] = (char(*)[16])(tBotBase + 0x24B568);
 	char(*tBotLabel2)[16] = (char(*)[16])(tBotBase + 0x24B59C);
 	memcpy(tBotLabel, "", strlen((LPCSTR)tBotLabel));
 	memcpy(tBotLabel2, "", strlen((LPCSTR)tBotLabel2));
-	MessageBoxA(NULL, (LPCSTR)tBotLabel, "Test", MB_OK);
 	return 0;
 }
 DWORD APIENTRY DllMain(
